2ac1b25baf4f6e5846784cecfe31870fc66d819a
hr677241-sketch <hr677241@gmail.com>
2026-06-12 22:01:31 +0500

    Update travel date from May to June 2026

--- check_seats.py
+++ check_seats.py
@@ -30,7 +30,7 @@ GMAIL_USER     = os.environ["GMAIL_USER"]
 GMAIL_PASSWORD = os.environ["GMAIL_APP_PASSWORD"]
 ALERT_TO       = "hr677241@gmail.com"
 
-TRAVEL_DATE       = "2026-05-23"
+TRAVEL_DATE       = "2026-06-23"
 FROM_STATION_CODE = "KCT"
 TO_STATION_CODE   = "SDK"
 FROM_STATION_NAME = "Karachi"
